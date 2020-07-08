ffe80c1c3431e336ce6316005b8b536fe265ecc2
AASHIR SAROYA <aashirsaroya@gmail.com>
2020-07-08 17:01:13 +0530

    Added another page and made meals screen better

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
 void main()=>runApp(MyApp());
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
       routes:{
         '/': (ctx) => CategoriesScreen(),
         '/category-meals': (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       } ,
     );
   }
--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 class MealItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
 
-  MealItem({@required this.title,@required this.imageUrl,@required this.duration,@required this.complexity,
+  MealItem({@required this.id,@required this.title,@required this.imageUrl,@required this.duration,@required this.complexity,
     @required this.affordability});
    String get complexityText
    {
@@ -43,15 +45,18 @@ class MealItem extends StatelessWidget {
      }
    }
 
-  void selectMeal()
+  void selectMeal(BuildContext ctx)
   {
+      Navigator.of(ctx).pushNamed(MealDetailScreen.routeName,
+      arguments: id,
+      );
 
   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15),),
       elevation: 4,
         margin: EdgeInsets.all(10),
