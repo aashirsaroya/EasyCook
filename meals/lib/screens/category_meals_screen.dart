4f81af98c5374b7e4d672c6158e813ca88548ec7
aashirsaroya <aashirsaroya@gmail.com>
2020-07-10 20:56:08 +0530

    Nailed the Filtering Logic:) & few styling upgrades

--- meals/lib/screens/category_meals_screen.dart
+++ meals/lib/screens/category_meals_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:meals/widgets/meal_item.dart';
-import '../dummy_data.dart';
 import '../models/meal.dart';
 class CategoryMealsScreen extends StatefulWidget {
   //final String categoryId;
   //final String categoryTitle;
   //  CategoryMealsScreen(this.categoryId,this.categoryTitle);
+  final List<Meal> availableMeals;
+  CategoryMealsScreen(this.availableMeals);
   @override
   _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
 }
@@ -27,7 +28,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
         final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,String>;
         categoryTitle = routeArgs['title'];
         final categoryId = routeArgs['id'];
-        displayedMeals = DUMMY_MEALS.where((meal){
+        displayedMeals = widget.availableMeals.where((meal){
           return meal.categories.contains(categoryId);
         }).toList();
         _loadedInitData = true;
--- meals/lib/screens/filters_screen.dart
+++ meals/lib/screens/filters_screen.dart
@@ -3,7 +3,9 @@ import 'package:meals/widgets/main_drawer.dart';
 class FiltersScreen extends StatefulWidget {
 
   static const routeName = '/filters';
-
+  final Function saveFilters;
+  final Map<String,bool> currentFilters;
+  FiltersScreen(this.currentFilters,this.saveFilters);
   @override
   _FiltersScreenState createState() => _FiltersScreenState();
 }
@@ -15,10 +17,33 @@ class _FiltersScreenState extends State<FiltersScreen> {
   var _lactoseFree = false;
 
   @override
+  void initState() {
+    _glutenFree = widget.currentFilters['gluten'];
+    _lactoseFree = widget.currentFilters['lactose'];
+    _vegan = widget.currentFilters['vegan'];
+    _vegetarian = widget.currentFilters['vegetarian'];
+     super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Filters!!'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: (){
+              final selectedFilters = {
+                'gluten':_glutenFree,
+                'lactose':_lactoseFree,
+                'vegan':_vegan,
+                'vegetarian':_vegetarian,
+              };
+              widget.saveFilters();
+            },
+          )
+        ],
       ),
       drawer: MainDrawer(),
       body: Column(
